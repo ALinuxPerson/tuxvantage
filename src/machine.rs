f2039f876e2e388298b39c0a0b6febee56a47bca
ALinuxPerson <alinuxperson@gmail.com>
2022-01-28 19:46:40 +0800

    Strip ansi escapes from error message if machine

--- src/machine.rs
+++ src/machine.rs
@@ -23,6 +23,10 @@ impl<S: Serialize> Machine<S> {
             .source
             .chain()
             .map(|error| error.to_string())
+            .map(strip_ansi_escapes::strip)
+            .map(|error| error.expect("failed to strip ansi escapes from error message"))
+            .map(String::from_utf8)
+            .map(|error| error.expect("error message contains invalid utf-8 after stripping it of ansi escapes"))
             .unique()
             .collect();
         Self::Failure {
